9d6ebbb2f9304dd76336d4c8610de938c609d211
lacornen <nlacorne@gmail.com>
2015-12-26 10:36:12 +0100

    Initial commit version

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# cleandata
+## "Getting and Cleaning Data" project
+
+This project is for [Getting and Cleaning Data Course] on platform Coursera. The objective of this project is to read the dataset  ["Human Activity Recognition Using Smartphones"], merge the data appropriately, compute relevant aggregate and output a tidy data set.
+
+**Operations performed by R script run_analysis.R:**
+Note that it requires the [plyr package](https://cran.r-project.org/web/packages/plyr/index.html) which can be downloaded from CRAN.
+
+**The R script [run_analysis.R] is performing the below operations.** 
+
+1. Download the zip file from location [https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip].
+2. Extract the files in the subdirectory "UCI HAR Dataset/"."
+
+3. Load in a dataframe data of the features label file "features.txt". That correspond to the name of each column in the X dataframe.
+
+4. Load in a dataframe data of the activity labels "activity_labels.txt".  
+
+3. Use the function GetTransFormSet to load the Test set which perform the following operation:
+        * Load in a dataframe df.x the features variables from the test set : UCI HAR Dataset/test/X_test.txt
+        * Load in a dataframe df.y , the activity id variables vector from the test set : UCI HAR Dataset/test/Y_test.txt
+        * Load in a dataframe df.subject the subject number file . "UCI HAR Dataset/test/subject_test.txt".
+        * Rename the column of features variable dataframe df.x according to label file features.txt.
+        * Transform features variable dataframe df.x in order to filter out column that are neither Mean or Standard Deviation.
+        * Tidy the column names using TidyVariableName function.
+        * Create a column with activity label that correpsond to activity id vector in df.y$V1.
+        * Create a column with subject number vector contained in file subject_test.txt.
+        
+4. Use the same function GetTransFormSet to load the Train Set.
+
+5. Bind the two datasets.
+
+6. Compute mean of each feature variable column aggregated by subject and acitivity.
+
+7. Ouput tidy data set.
+                             
+
+**The R script [run_analysis.R] output is a tidy data set, [tidy_data_output.txt](tidy_data_output.txt).**
+
+Please find more about the data  in the [codebook](CodeBook.md).
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,73 @@
+library(plyr)
+
+# Function to ouput tidier variable name
+TidyVariableName <- function(var.name) {
+        var.tmp <- var.name
+        var.tmp <- gsub("-mean\\()","Mean",var.tmp)
+        var.tmp <- gsub("-std\\()","StdDev", var.tmp)
+        var.tmp <- gsub("^t","time", var.tmp)
+        var.tmp <- gsub("^f","freq", var.tmp)
+        var.tmp <- gsub("BodyBody","Body", var.tmp)
+        var.tmp <- gsub("Mag","Magnitude", var.tmp)
+        var.tmp <- gsub("Acc","Acceleration", var.tmp)
+        var.tmp <- gsub("-X","-X", var.tmp)
+        var.tmp <- gsub("-Y","-Y", var.tmp)
+        var.tmp <- gsub("-Z","-Z", var.tmp)
+        return(var.tmp)
+}
 
 GetTransFormSet  <- function(filename.x, filename.y, filename.subject,
                              df.features, df.activity.labels){
   
+        # Read x and y file
         df.x <- read.table(filename.x) 
         
         df.y <- read.table(filename.y) 
         
+        #df.activity.labels to defactor to come back other wise wrong merging to redo
+        # Read subject file
         df.subject <- read.table(filename.subject)
         
-        for(i in 1:561) {
+        # Rename variable using features table description
+        for(i in 1:nrow(df.features)) {
                 names(df.x)[df.features[i,'V1']] <- as.character(df.features[i,'V2'])
         }
+        
+        # Keep only variable correspondint to mean or standard deviation
         df.sub<- df.x[grep(".*(mean|std)\\(\\).*",names(df.x))]
         
-        df.sub['subject'] <- df.subject$V1
-        df.sub['activity_int'] <- df.y$V1
+        # Change  variable name to be more tidy
+        colname.sub <-  colnames(df.sub) 
         
-        df.sub <- merge(df.sub, df.activity.labels, by.x='activity_int', by.y='V1', all.x=TRUE)
+        for (i in 1:length(colname.sub)) {
+                colname.sub[i] <- TidyVariableName(colname.sub[i])
+        }
+        colnames(df.sub) <- colname.sub
         
-        colnames(df.sub)[colnames(df.sub) == 'V2'] <- 'activity'
+        
+        # define activity label column  by matching activity id number in df.y and label description 
+        df.sub['activity'] <- factor(df.y$V1, levels=df.activity.labels$V1, 
+                                     labels=df.activity.labels$V2)
+        
+        # define subject column
+        df.sub['subject'] <- as.factor(df.subject$V1)
         
         return(df.sub)
         
 } 
+
+file.zip <- "getdata_projectfiles_UCI HAR Dataset.zip"
+data.dir <- "UCI HAR Dataset"
+
+file.url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+if (!file.exists(data.dir) & !file.exists(file.zip)) {
+        download.file(file.url, file.zip)
+} 
+
+if(!file.exists(data.dir)) {
+        unzip(file.zip)
+}
 
 df.features <- read.table("UCI HAR Dataset/features.txt") 
 df.activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
@@ -35,12 +80,23 @@ df.test <- GetTransFormSet(filename.x="UCI HAR Dataset/test/X_test.txt",
                            df.activity.labels=df.activity.labels)
 
 
-df.x <- read.table("UCI HAR Dataset/test/X_test.txt") 
+df.train <- GetTransFormSet(filename.x="UCI HAR Dataset/train/X_train.txt",
+                            filename.y="UCI HAR Dataset/train/Y_train.txt",
+                            filename.subject="UCI HAR Dataset/train/subject_train.txt",
+                            df.features=df.features,
+                            df.activity.labels=df.activity.labels)
 
-df.y <- read.table("UCI HAR Dataset/test/Y_test.txt") 
+
+# Bind the two sets 
+
+df.full <- rbind(df.train, df.test)
 
 
+# Perform mean on each column grouped by subject and activity using ddply
+
+df.avg <- ddply(df.full, .(subject,activity), colwise(mean))
+
+# Output tidy dataset.
+write.table(df.avg, "tidy_data_output.txt", row.name=FALSE)
 
 
-
-summary(df.x.sub)
